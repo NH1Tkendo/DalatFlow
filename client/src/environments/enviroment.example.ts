bc0741d3b505e329920ca1e2444c54c4c0d7e86d
NH1Tkendo <ngobatai1711@gmail.com>
2026-04-29 00:08:44 +0700

    chore: Thêm các file enviroment vào .gitignore và thêm file enviroment.example.ts
    Signed-off-by: NH1Tkendo <ngobatai1711@gmail.com>

---
+++ client/src/environments/enviroment.example.ts
@@ -0,0 +1,10 @@
+// environment.example.ts
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:3000',
+  firebaseConfig: {
+    apiKey: 'YOUR_API_KEY_HERE',
+    authDomain: 'YOUR_AUTH_DOMAIN_HERE',
+    // ...
+  },
+};
